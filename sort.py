3bbc6db7f5a4f427440fad3483f43d9979fe50b4
WillChilds-Klein <willck93@gmail.com>
2017-07-09 03:19:51 -0400

    flake8 compliance, other bs.

--- sort.py
+++ sort.py
@@ -1,6 +1,5 @@
 import sys
 import time
-from random import randint
 from math import floor
 from collections import Counter
 
@@ -20,10 +19,10 @@ def main():
     ms_millis, ms_sorted = time_sort(ms, nums)
     ns_millis, ns_sorted = time_sort(ns, nums)
     bs_millis, bs_sorted = time_sort(bs, nums)
-    print 'QSRT: {} ms \t {}'.format(qs_millis, qs_sorted if not infile else '')
-    print 'MSRT: {} ms \t {}'.format(ms_millis, ms_sorted if not infile else '')
-    print 'NSRT: {} ms \t {}'.format(ns_millis, ns_sorted if not infile else '')
-    print 'BSRT: {} ms \t {}'.format(bs_millis, bs_sorted if not infile else '')
+    print 'QSRT: {}ms \t {}'.format(qs_millis, qs_sorted if not infile else '')
+    print 'MSRT: {}ms \t {}'.format(ms_millis, ms_sorted if not infile else '')
+    print 'NSRT: {}ms \t {}'.format(ns_millis, ns_sorted if not infile else '')
+    print 'BSRT: {}ms \t {}'.format(bs_millis, bs_sorted if not infile else '')
 
 
 def time_sort(func, nums):
@@ -37,8 +36,7 @@ def validate(nums, sorted_nums):
     if len(nums) != len(sorted_nums):
         raise Exception('sorted list not same length as original!')
     if Counter(nums) != Counter(sorted_nums):
-        raise Exception('sorted list and original do not contain same elements!')
-
+        raise Exception('sorted list and original have different elements!')
     prev = sorted_nums.pop(0)
     for item in sorted_nums:
         if prev > item:
@@ -47,13 +45,15 @@ def validate(nums, sorted_nums):
 
 
 def qs(nums):
-    if nums == []: return []
+    if nums == []:
+        return nums
     return qs([num for num in nums[1:] if num < nums[0]]) + nums[:1] + \
            qs([num for num in nums[1:] if num >= nums[0]])
 
 
 def ms(nums):
-    if len(nums) == 1 or nums == []: return nums
+    if len(nums) == 1 or nums == []:
+        return nums
     p = int(floor(len(nums) / 2))
     return merge(ms(nums[:p]), ms(nums[p:]))
 
